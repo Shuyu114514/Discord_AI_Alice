5d7a5d35f377e511a3ad0fe91c453664544d082e
Shuyu114514 <108236326+Shuyu114514@users.noreply.github.com>
2022-10-24 00:05:28 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import discord
 import os
 import talking
 
-TOKEN = 'MTAzMjg3NTQxOTIxMDM1ODc4NA.Gh6heV.pvbaztnuyA4R0G0CgHY8hjE_28BCUPi1ONT-10'
+# Please create a txt file named Alice_Token and write the token at the first line
+f = open('Alice_Token.txt', 'r')
+lines = f.readlines()
+TOKEN = lines[0]
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
