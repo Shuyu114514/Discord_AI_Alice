a127ee79943ad9deb8f7e9c5e3fe4f74720e4c21
lhr001fly <33202959+lhr001fly@users.noreply.github.com>
2022-10-24 00:47:03 -0400

    Update games.py

--- games.py
+++ games.py
@@ -14,7 +14,7 @@ async def random_num(message):
 		message_send = 'My master, I generated a random number between 0 and 1 for you: %f' % random.random()
 		await message.channel.send(message_send)
 		
-	if re.match(message.content, reg_ndn):
+	if re.match(reg_ndn, message.content):
 		word_ndn = message.content[2:]
 		lst_ndn = word_ndn.split('d')
 		num_dice = int(lst_ndn[0])
